cdb1792d2abbf5db9b8feb242a7a37ea5228b664
Tony Tang <tonytang@uber.com>
2020-02-26 15:16:31 -0800

    Upgrade leak canary to 2.2

--- app/src/main/java/demo/leakcanary/srain/in/leakcanarydemo/ExampleApplication.java
+++ app/src/main/java/demo/leakcanary/srain/in/leakcanarydemo/ExampleApplication.java
@@ -2,14 +2,11 @@ package demo.leakcanary.srain.in.leakcanarydemo;
 
 import android.app.Application;
 
-import com.squareup.leakcanary.LeakCanary;
-
 public class ExampleApplication extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
-        LeakCanary.install(this);
     }
 
 }
